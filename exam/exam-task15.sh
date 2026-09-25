86983143f1eacb5fd3a2ad3d3e15570fa5c5469f
Chris Twinn <git@ct.twinn.co.uk>
2026-09-25 10:46:24 +0100

    Update exam-task15.sh to include remote search
    Exam asks for us to accomplish the task on server2. 
    Update to execute check on server2 and retrieve the file from server2

--- exam/exam-task15.sh
+++ exam/exam-task15.sh
@@ -1,6 +1,7 @@
 #find /etc -type f -exec grep -Ri 'administrator' {} \; > /root/lab15.check &>/dev/null
-find /etc -type f -readable -print0 | xargs -0 -r grep -iH 'administrator' > /root/lab15.check
 
+ssh server2 find /etc -type f -readable -print0 | xargs -0 -r grep -i 'administrator' 2>/dev/null > /root/lab15.check
+scp server2:/root/administratorfiles.txt /root/administratorfiles.txt
 
 if diff -u <(sort /root/lab15.check) <(sort /root/administratorfiles.txt) &>/dev/null
 then
